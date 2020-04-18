6a42344c60c1a1bab0bcdce65081704d0920abc5
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-18 16:18:37 +0200

    Update TSX, add placeholderbg1 to dev level

---
+++ tiled/tsx/placeholderbg1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="placeholderbg1" tilewidth="4" tileheight="4" tilecount="880" columns="40">
+ <image source="../../resources/spritesheets/tilesets/placeholderbg1.png" width="160" height="90"/>
+</tileset>
