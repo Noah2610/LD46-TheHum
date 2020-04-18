d88dab92a0b3f971dd74220e34ba5adcc1516149
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-18 16:46:59 +0200

    Playing with level

--- tiled/tsx/dev.tsx
+++ tiled/tsx/dev.tsx
@@ -2,4 +2,7 @@
 <tileset version="1.2" tiledversion="1.3.3" name="dev" tilewidth="4" tileheight="4" tilecount="4" columns="2">
  <image source="../../resources/spritesheets/tilesets/dev.png" width="8" height="8"/>
  <tile id="0" type="Ground"/>
+ <tile id="1" type="Ground"/>
+ <tile id="2" type="Ground"/>
+ <tile id="3" type="Ground"/>
 </tileset>
