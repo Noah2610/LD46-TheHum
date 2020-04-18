f2588a38464adace6a2d7eca1fe15d89c0a9b6b1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-18 15:54:32 +0200

    Add player jump

--- src/systems/control_player.rs
+++ src/systems/control_player.rs
@@ -1,4 +1,5 @@
 use super::system_prelude::*;
+use deathframe::physics::query::prelude::*;
 
 #[derive(Default)]
 pub struct ControlPlayerSystem;
@@ -8,16 +9,41 @@ impl<'a> System<'a> for ControlPlayerSystem {
         Read<'a, InputManager<IngameBindings>>,
         ReadStorage<'a, Player>,
         WriteStorage<'a, Movable>,
+        ReadStorage<'a, Collider<CollisionTag>>,
     );
 
     fn run(
         &mut self,
-        (input_manager, player_store, mut movable_store): Self::SystemData,
+        (
+            input_manager,
+            player_store,
+            mut movable_store,
+            collider_store,
+        ): Self::SystemData,
     ) {
-        for (_, movable) in (&player_store, &mut movable_store).join() {
+        for (_, movable, collider) in
+            (&player_store, &mut movable_store, &collider_store).join()
+        {
+            // MOVE
             if let Some(x) = input_manager.axis_value(IngameAxis::MoveX) {
                 if x != 0.0 {
                     movable.add_action(MoveAction::Walk(x));
+                }
+            }
+
+            let on_ground = {
+                use deathframe::physics::query::exp::prelude_variants::*;
+                collider
+                    .query::<FindQuery<CollisionTag>>()
+                    .exp(&And(vec![IsSide(Bottom), IsTag(CollisionTag::Solid)]))
+                    .run()
+                    .is_some()
+            };
+
+            // JUMP
+            if on_ground {
+                if input_manager.is_down(IngameAction::Jump) {
+                    movable.add_action(MoveAction::Jump);
                 }
             }
         }
