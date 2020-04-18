6598cfdaa1a9b2230159607256ee463e29c480f6
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-18 14:25:27 +0200

    Add dev spritesheet, map, tileset

---
+++ tiled/tsx/dev.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="dev" tilewidth="4" tileheight="4" tilecount="4" columns="2">
+ <image source="../../resources/spritesheets/tiles/dev.png" width="8" height="8"/>
+ <tile id="0" type="Ground"/>
+</tileset>
