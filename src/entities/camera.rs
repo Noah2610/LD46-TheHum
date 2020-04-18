92aa39b167f8f2d0cb3cfb3829036b5f625d4765
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-18 16:09:03 +0200

    Remove camera size setting, camera uses level size

--- src/entities/camera.rs
+++ src/entities/camera.rs
@@ -14,7 +14,7 @@ pub fn init_camera(world: &mut World, level_size: Size) -> Entity {
     let mut transform = Transform::default();
     transform.set_translation_z(camera_settings.z);
 
-    let size = Size::from(camera_settings.size);
+    let size = level_size;
 
     let camera = Camera::standard_2d(size.w, size.h);
     let mut camera_ortho =
@@ -29,12 +29,12 @@ pub fn init_camera(world: &mut World, level_size: Size) -> Entity {
 
     world
         .create_entity()
+        .with(Confined::from(
+            Rect::from(&size)
+                .with_offset(&Point::new(half_size.w, half_size.h)),
+        ))
         .with(transform)
         .with(size)
         .with(camera)
-        .with(Confined::from(Rect::from(&level_size).with_offset(&{
-            let half_size = level_size.half();
-            Point::new(half_size.w, half_size.h)
-        })))
         .build()
 }
--- src/settings/camera_settings.rs
+++ src/settings/camera_settings.rs
@@ -1,9 +1,6 @@
 // resources/settings/camera.ron
 
-use crate::components::prelude::Size;
-
 #[derive(Clone, Deserialize)]
 pub struct CameraSettings {
-    pub z:    f32,
-    pub size: Size,
+    pub z: f32,
 }
