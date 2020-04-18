1f9e129570485898fbf896e6078ee2bb188497bc
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-18 14:11:15 +0200

    Add BaseFriction and Gravity to player

--- src/settings/player_settings.rs
+++ src/settings/player_settings.rs
@@ -4,8 +4,10 @@ use crate::components::prelude::*;
 
 #[derive(Clone, Deserialize)]
 pub struct PlayerSettings {
-    pub z:       f32,
-    pub size:    Size,
-    pub hitbox:  Hitbox,
-    pub movable: Movable,
+    pub z:             f32,
+    pub size:          Size,
+    pub hitbox:        Hitbox,
+    pub movable:       Movable,
+    pub base_friction: BaseFriction,
+    pub gravity:       Gravity,
 }
