44150f35f467694c5280c4c739db8251a172ce43
emu <31223078+hoichael@users.noreply.github.com>
2020-04-19 19:05:00 +0200

    Changes to dev2 + created tsx for bg variations

---
+++ tiled/tsx/bg3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="bgvar2" tilewidth="4" tileheight="4" tilecount="9216" columns="128">
+ <image source="../../resources/spritesheets/tilesets/bgvar2.png" width="512" height="288"/>
+</tileset>
