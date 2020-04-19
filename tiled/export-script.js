6876beb0a45ad1defddc63339bec0e7c712219eb
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-19 23:14:45 +0200

    Add function convertTypeString to tiled export script
    Copied from deathfloor

--- tiled/export-script.js
+++ tiled/export-script.js
@@ -62,6 +62,49 @@
         };
     }
 
+    // Converts the given type string into an appropriate type,
+    // that can be parsed as an enum with optional fields by serde.
+    // Examples:
+    //     "Player"
+    //         ...converts to...
+    //     "Player"
+    //
+    //     "Enemy(Normal)"
+    //         ...converts to...
+    //     { "Enemy": "Normal" }
+    //
+    //     "BigEnemy(Tank, Large)"
+    //         ...converts to...
+    //     { "Enemy": ["Tank", "Large"] }
+    function convertTypeString(typeString) {
+        let returnType = typeString;
+        const match = typeString.match(/^\s*(\w+)(\((.*)\))?\s*$/) || [];
+        const ident = match[1];
+        const fieldsString = match[3];
+        if (ident) {
+            if (fieldsString) {
+                const fields = fieldsString.split(/\s*,\s*/);
+                if (fields.length === 0) {
+                    returnType = {
+                        [ident]: null,
+                    };
+                } else if (fields.length === 1) {
+                    returnType = {
+                        [ident]: fields[0],
+                    };
+                } else if (fields.length > 1) {
+                    returnType = {
+                        [ident]: fields,
+                    };
+                }
+            } else {
+                returnType = ident;
+            }
+        }
+
+        return returnType;
+    }
+
     function getHitboxFrom(objectGroup, layer) {
         const hitboxRects = [];
         const tileSize = {
@@ -131,7 +174,7 @@
                     );
 
                     tileOutput.id = tile.id;
-                    tileOutput.type = tile.type;
+                    tileOutput.type = convertTypeString(tile.type);
                     tileOutput.ts = tilesetName;
                     tileOutput.pos = pos;
                     tileOutput.props = Object.assign({}, layerProps, tileProps);
@@ -164,7 +207,7 @@
                 mapSize,
             );
             const objectOutput = {
-                type: object.type,
+                type: convertTypeString(object.type),
                 pos: pos,
                 size: {
                     w: object.width,
