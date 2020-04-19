d91ebae4513bfce19d4ce9d337f272cccc23c20f
emu <31223078+hoichael@users.noreply.github.com>
2020-04-19 14:25:22 +0200

    Additions to dev2, minor changes to testtiles1, created bg1 and tileset1 tsx

---
+++ tiled/tsx/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tileset1" tilewidth="4" tileheight="4" tilecount="1024" columns="32">
+ <image source="../../resources/spritesheets/tilesets/testtiles1.png" width="128" height="128"/>
+</tileset>
