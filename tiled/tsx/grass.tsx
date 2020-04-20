314f3363b264f35cb5b6cbf343103b354fca5b1e
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-21 01:09:51 +0200

    Add generated tiles reactive animations config

--- tiled/tsx/grass.tsx
+++ tiled/tsx/grass.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="grass" tilewidth="4" tileheight="4" tilecount="108" columns="6">
  <image source="../../resources/spritesheets/tilesets/grasstiles.png" width="24" height="72"/>
+ <tile id="0" type="Grass_0"/>
+ <tile id="1" type="Grass_1"/>
+ <tile id="6" type="Grass_6"/>
+ <tile id="7" type="Grass_7"/>
 </tileset>
