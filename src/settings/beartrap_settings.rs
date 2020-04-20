56c439343e119744a724aec2b3cf673ae1c178be
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-20 20:07:26 +0200

    Add AnimationsContainer to beartrap, play BeartrapHit animation on beartrap when hit
    Closes #46

--- src/settings/beartrap_settings.rs
+++ src/settings/beartrap_settings.rs
@@ -1,10 +1,12 @@
 // resources/settings/beartrap.ron
 
 use crate::components::prelude::*;
+use crate::resources::prelude::AnimationKey;
 
 #[derive(Clone, Deserialize)]
 pub struct BeartrapSettings {
-    pub size:     Size,
-    pub hitbox:   Hitbox,
-    pub beartrap: Beartrap,
+    pub size:       Size,
+    pub hitbox:     Hitbox,
+    pub beartrap:   Beartrap,
+    pub animations: AnimationsContainer<AnimationKey>,
 }
