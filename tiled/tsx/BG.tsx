291716409bc93336781638afd49a3b909a2d45a8
emu <31223078+hoichael@users.noreply.github.com>
2020-04-20 18:11:34 +0200

    Resized bg2 + changes to level

---
+++ tiled/tsx/BG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="BG" tilewidth="4" tileheight="4" tilecount="20736" columns="192">
+ <image source="../../resources/spritesheets/tilesets/bgvar2.png" width="768" height="432"/>
+</tileset>
